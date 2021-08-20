66fa2ee69088b168811096b500df7bba09f48bc8
MatievisTheKat <matievisthekat@gmail.com>
2021-08-20 13:11:25 +0200

    add env variables

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,7 @@
+import { config } from "dotenv";
 import { ApplicationConfig, Application } from "./application";
+
+config();
 
 export * from "./application";
 
@@ -9,7 +12,6 @@ export async function main(options: ApplicationConfig = {}) {
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
 
   return app;
 }
