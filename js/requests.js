71368dc53deafc38a0afd58f1a8f23aac4de7754
matievisthekat <matievisthekat@gmail.com>
2021-12-03 23:55:27 +0200

    add request logging and displaying

--- js/requests.js
+++ js/requests.js
@@ -1,9 +1,13 @@
 "use strict";
+var tableBody = document.querySelector('tbody#requests');
 var entries = performance.getEntriesByType('resource');
 var requests = entries.map(function (e) { return ({
-    url: e.name,
+    url: e.name.split(window.location.host).pop(),
     duration: e.duration,
     size: e.transferSize
 }); });
-console.log(requests);
+for (var _i = 0, requests_1 = requests; _i < requests_1.length; _i++) {
+    var req = requests_1[_i];
+    tableBody.innerHTML += "\n    <tr class=\"request\">\n      <td class=\"size\">".concat(req.size.toFixed(0), "B</td>\n      <td class=\"url\">").concat(req.url, "</td>\n      <td class=\"duration\">(").concat(req.duration.toFixed(1), "ms)</td>\n    </tr>\n  ");
+}
 //# sourceMappingURL=requests.js.map
--- ts/requests.ts
+++ ts/requests.ts
@@ -1,8 +1,17 @@
+const tableBody = document.querySelector('tbody#requests') as HTMLDivElement;
 const entries = performance.getEntriesByType('resource');
 const requests = entries.map((e) => ({
-  url: e.name,
+  url: e.name.split(window.location.host).pop(),
   duration: e.duration,
   size: (e as PerformanceResourceTiming).transferSize,
 }));
 
-console.log(requests);
+for (const req of requests) {
+  tableBody.innerHTML += `
+    <tr class="request">
+      <td class="size">${req.size.toFixed(0)}B</td>
+      <td class="url">${req.url}</td>
+      <td class="duration">(${req.duration.toFixed(1)}ms)</td>
+    </tr>
+  `;
+}
