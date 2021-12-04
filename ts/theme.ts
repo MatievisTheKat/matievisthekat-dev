be5c544c8d8cc6d4d250524b49c8d932fccb313b
matievisthekat <matievisthekat@gmail.com>
2021-12-04 10:53:28 +0200

    remove fetch to example.com

--- ts/theme.ts
+++ ts/theme.ts
@@ -13,5 +13,3 @@ const theme = (window.localStorage.getItem('theme') || 'light') as Theme;
 const colourblind = (window.localStorage.getItem('colourblind') || 'none') as Colourblind;
 
 document.querySelector('body')?.classList.add(colourblind, theme);
-
-fetch('https://example.com', { mode: 'no-cors' });
