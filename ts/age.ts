85980518fb857d0c7e3e3c56b7035b9f88262d54
matievisthekat <matievisthekat@gmail.com>
2021-12-04 21:08:08 +0200

    display age counter

--- ts/age.ts
+++ ts/age.ts
@@ -1,8 +1,11 @@
-const el = document.querySelector('#age');
+const el = document.querySelector('span#age') as HTMLSpanElement | null;
 if (el) {
   const birthday = new Date(2005, 4, 13);
 
   setInterval(() => {
     const diff = Date.now() - birthday.getTime();
+    const yearInMs = 3.154e10;
+    const age = (diff / yearInMs).toLocaleString(undefined, { maximumFractionDigits: 10, minimumFractionDigits: 10 });
+    el.innerText = age;
   }, 1);
-} else console.warn('[age] #age element not found');
+} else console.warn('[age] span#age element not found');
