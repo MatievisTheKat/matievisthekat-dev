2afce3f1ed3163b2dd0b597327960604672d4fbd
matievisthekat <matievisthekat@gmail.com>
2021-12-07 11:48:10 +0200

    remove built files

--- js/components/theme.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-const theme = (window.localStorage.getItem('theme') || 'light');
-const colourblind = (window.localStorage.getItem('colourblind') || 'none');
-(_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add(colourblind, theme);
-//# sourceMappingURL=theme.js.map
