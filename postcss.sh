f090471c7e428c8d520289ac1b8821433ef33a1f
matievisthekat <matievisthekat@gmail.com>
2021-12-07 10:30:31 +0200

    add postcss-reporter

--- postcss.sh
+++ postcss.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-postcss css/raw/**/*.css --base css/raw --dir css/prod --map --use autoprefixer postcss-preset-env cssnano
+postcss css/raw/**/*.css --base css/raw --dir css/prod --map --use autoprefixer postcss-preset-env cssnano postcss-reporter
