e61d2b4f74f23a657730d4ad9d842153dc57f341
matievisthekat <matievisthekat@gmail.com>
2021-12-08 11:32:39 +0200

    add build files back

--- js/components/theme.js
+++ js/components/theme.js
@@ -3,4 +3,12 @@ var _a;
 const theme = (window.localStorage.getItem('theme') || 'light');
 const colourblind = (window.localStorage.getItem('colourblind') || 'none');
 (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add(colourblind, theme);
+function toggleDropdown(mode) {
+    const dropdown = document.querySelector(`div#${mode}-dropdown`);
+    if (dropdown) {
+        dropdown.classList.toggle('hide');
+    }
+    else
+        console.warn(`[toggleDropdown(${mode})] div#${mode}-dropdown not found`);
+}
 //# sourceMappingURL=theme.js.map
