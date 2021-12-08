fe6edf86a08690448ebd7f70dc149cd2ce88c646
matievisthekat <matievisthekat@gmail.com>
2021-12-08 11:33:34 +0200

    move theme code into one file

--- ts/components/theme.ts
+++ ts/components/theme.ts
@@ -3,14 +3,9 @@ const colourblind = (window.localStorage.getItem('colourblind') || 'none') as Co
 
 document.querySelector('body')?.classList.add(colourblind, theme);
 
-const tb = document.querySelector('button#colourblind') as HTMLButtonElement | null;
-const themeDropdown = document.querySelector('div#colourblind-dropdown') as HTMLDivElement | null;
-if (cb && themeDropdown) {
-  cb.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    if (themeDropdown.classList.contains('hidden')) {
-      themeDropdown.classList.remove('hidden');
-    } else themeDropdown.classList.add('hidden');
-  });
-} else console.warn('[colourblind.button] Elements not found');
+function toggleDropdown(mode: 'colourblind' | 'theme') {
+  const dropdown = document.querySelector(`div#${mode}-dropdown`) as HTMLDivElement | null;
+  if (dropdown) {
+    dropdown.classList.toggle('hide');
+  } else console.warn(`[toggleDropdown(${mode})] div#${mode}-dropdown not found`);
+}
