4e5030907aeba8f8bd1056dbd49e65f67d8df51b
matievisthekat <matievisthekat@gmail.com>
2021-12-09 19:54:42 +0200

    remove built files

--- js/common/hasParentOfClass.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function hasSomeParentOfClass(element, classname) {
-    var _a;
-    if (element.classList.contains(classname))
-        return true;
-    return (_a = (element.parentElement && hasSomeParentOfClass(element.parentElement, classname))) !== null && _a !== void 0 ? _a : false;
-}
-//# sourceMappingURL=hasParentOfClass.js.map
