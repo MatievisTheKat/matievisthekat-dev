9e757eac71d7aa0c93c37170e6143aa5f24a7b4e
matievisthekat <matievisthekat@gmail.com>
2021-12-09 13:44:53 +0200

    use classList.contains instead of className.split(' ')

--- js/common/hasParentOfClass.js
+++ js/common/hasParentOfClass.js
@@ -1,7 +1,7 @@
 "use strict";
 function hasSomeParentOfClass(element, classname) {
     var _a;
-    if (element.className.split(' ').indexOf(classname) >= 0)
+    if (element.classList.contains(classname))
         return true;
     return (_a = (element.parentElement && hasSomeParentOfClass(element.parentElement, classname))) !== null && _a !== void 0 ? _a : false;
 }
--- ts/common/hasParentOfClass.ts
+++ ts/common/hasParentOfClass.ts
@@ -1,4 +1,4 @@
 function hasSomeParentOfClass(element: HTMLElement, classname: string): boolean {
-  if (element.className.split(' ').indexOf(classname) >= 0) return true;
+  if (element.classList.contains(classname)) return true;
   return (element.parentElement && hasSomeParentOfClass(element.parentElement, classname)) ?? false;
 }
