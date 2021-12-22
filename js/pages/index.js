2c3a2bc9db25193521f0b8e6013a87829e974763
matievisthekat <matievisthekat@gmail.com>
2021-12-22 13:52:06 +0200

    dont rotate page sections on scroll

--- js/pages/index.js
+++ js/pages/index.js
@@ -3,9 +3,7 @@ const sections = document.querySelectorAll('section.page');
 sections.forEach((sect, i) => {
     document.body.addEventListener('scroll', () => {
         const scrollValue = Math.fround(sect.offsetTop - document.body.scrollTop);
-        const style = sect.style;
-        style.setProperty('--translate', `${scrollValue / 4}px`);
-        style.setProperty('--rotate', `${-scrollValue / 15}deg`);
+        sect.style.setProperty('--translate', `${scrollValue / 4}px`);
     });
 });
 //# sourceMappingURL=index.js.map
--- ts/pages/index.ts
+++ ts/pages/index.ts
@@ -2,8 +2,6 @@ const sections = document.querySelectorAll('section.page');
 sections.forEach((sect, i) => {
   document.body.addEventListener('scroll', () => {
     const scrollValue = Math.fround((sect as HTMLElement).offsetTop - document.body.scrollTop);
-    const style = (sect as HTMLElement).style;
-    style.setProperty('--translate', `${scrollValue / 4}px`);
-    style.setProperty('--rotate', `${-scrollValue / 15}deg`);
+    (sect as HTMLElement).style.setProperty('--translate', `${scrollValue / 4}px`);
   });
 });
