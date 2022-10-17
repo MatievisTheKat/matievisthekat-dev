9e344de230cecc92ff2b3c949d53fbeaca0f222e
matievisthekat <matievisthekat@gmail.com>
2022-10-17 19:04:01 +0200

    remove hasParentOfClass util script

--- ts/common/hasParentOfClass.ts
+++
@@ -1,4 +0,0 @@
-function hasSomeParentOfClass(element: HTMLElement, classname: string): boolean {
-  if (element.classList.contains(classname)) return true;
-  return (element.parentElement && hasSomeParentOfClass(element.parentElement, classname)) ?? false;
-}
