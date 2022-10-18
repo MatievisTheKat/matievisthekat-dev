eddd7e065cb025515625f9a7d8ba34f0ba3294d2
matievisthekat <matievisthekat@gmail.com>
2022-10-18 15:23:56 +0200

    add tooltip to social anchors

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,9 +1,29 @@
 "use strict";
-const sections = document.querySelectorAll('section.page');
-sections.forEach((sect, i) => {
-    document.body.addEventListener('scroll', () => {
-        const scrollValue = Math.fround(sect.offsetTop - document.body.scrollTop);
-        sect.style.setProperty('--translate', `${scrollValue / 4}px`);
+const socials = document.querySelectorAll('div#external-links > a');
+if (socials.length > 0) {
+    socials.forEach((a) => {
+        var _a;
+        const title = (_a = a.attributes.getNamedItem('data-title')) === null || _a === void 0 ? void 0 : _a.textContent;
+        const rect = a.getBoundingClientRect();
+        if (title) {
+            const tooltip = document.createElement('div');
+            tooltip.classList.add('tooltip');
+            tooltip.id = `tooltip-${title.toLowerCase()}`;
+            tooltip.textContent = title;
+            tooltip.style.top = `${rect.top - 3}px`;
+            tooltip.style.left = `${rect.left}px`;
+            tooltip.style.height = `${rect.height}px`;
+            tooltip.style.width = `${rect.width}px`;
+            const addTooltip = () => {
+                a.appendChild(tooltip);
+            };
+            const removeTooltip = () => {
+                a.removeChild(tooltip);
+            };
+            a.addEventListener('mouseover', addTooltip);
+            a.addEventListener('mouseout', removeTooltip);
+            tooltip.addEventListener('mouseover', addTooltip);
+        }
     });
-});
+}
 //# sourceMappingURL=index.js.map
--- ts/pages/index.ts
+++ ts/pages/index.ts
@@ -1,7 +1,33 @@
-const sections = document.querySelectorAll('section.page');
-sections.forEach((sect, i) => {
-  document.body.addEventListener('scroll', () => {
-    const scrollValue = Math.fround((sect as HTMLElement).offsetTop - document.body.scrollTop);
-    (sect as HTMLElement).style.setProperty('--translate', `${scrollValue / 4}px`);
+const socials = document.querySelectorAll('div#external-links > a');
+if (socials.length > 0) {
+  socials.forEach((a) => {
+    const title = a.attributes.getNamedItem('data-title')?.textContent;
+    const rect = a.getBoundingClientRect();
+
+    // a.querySelector('button')?.addEventListener('mouseover', (_) => a.classList.add('hover'));
+    // a.querySelector('button')?.addEventListener('mouseout', (_) => a.classList.remove('hover'));
+
+    if (title) {
+      const tooltip = document.createElement('div');
+      tooltip.classList.add('tooltip');
+      tooltip.id = `tooltip-${title.toLowerCase()}`;
+      tooltip.textContent = title;
+      tooltip.style.top = `${rect.top - 3}px`; // adjusting for the slight padding the icons have at the bottom
+      tooltip.style.left = `${rect.left}px`;
+      tooltip.style.height = `${rect.height}px`;
+      tooltip.style.width = `${rect.width}px`;
+
+      const addTooltip = () => {
+        a.appendChild(tooltip);
+      };
+
+      const removeTooltip = () => {
+        a.removeChild(tooltip);
+      };
+
+      a.addEventListener('mouseover', addTooltip);
+      a.addEventListener('mouseout', removeTooltip);
+      tooltip.addEventListener('mouseover', addTooltip);
+    }
   });
-});
+}
