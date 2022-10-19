6499bc4689ef9ea68d33194c88e1002016b56f0f
matievisthekat <matievisthekat@gmail.com>
2022-10-19 09:58:02 +0200

    fix tooltip

--- js/pages/index.js
+++ js/pages/index.js
@@ -4,25 +4,25 @@ if (socials.length > 0) {
     socials.forEach((a) => {
         var _a;
         const title = (_a = a.attributes.getNamedItem('data-title')) === null || _a === void 0 ? void 0 : _a.textContent;
-        const rect = a.getBoundingClientRect();
         if (title) {
             const tooltip = document.createElement('div');
             tooltip.classList.add('tooltip');
             tooltip.id = `tooltip-${title.toLowerCase()}`;
             tooltip.textContent = title;
-            tooltip.style.top = `${rect.top - 3}px`;
-            tooltip.style.left = `${rect.left}px`;
-            tooltip.style.height = `${rect.height}px`;
-            tooltip.style.width = `${rect.width}px`;
             const addTooltip = () => {
+                if (a.querySelector('div.tooltip') !== null)
+                    return;
+                const rect = a.getBoundingClientRect();
+                tooltip.style.top = `${rect.top + rect.height}px`;
+                tooltip.style.left = `${rect.left}px`;
+                tooltip.style.width = `${rect.width}px`;
                 a.appendChild(tooltip);
             };
             const removeTooltip = () => {
                 a.removeChild(tooltip);
             };
             a.addEventListener('mouseover', addTooltip);
-            a.addEventListener('mouseout', removeTooltip);
-            tooltip.addEventListener('mouseover', addTooltip);
+            a.addEventListener('mouseleave', removeTooltip);
         }
     });
 }
--- ts/pages/index.ts
+++ ts/pages/index.ts
@@ -2,22 +2,20 @@ const socials = document.querySelectorAll('div#external-links > a');
 if (socials.length > 0) {
   socials.forEach((a) => {
     const title = a.attributes.getNamedItem('data-title')?.textContent;
-    const rect = a.getBoundingClientRect();
-
-    // a.querySelector('button')?.addEventListener('mouseover', (_) => a.classList.add('hover'));
-    // a.querySelector('button')?.addEventListener('mouseout', (_) => a.classList.remove('hover'));
 
     if (title) {
       const tooltip = document.createElement('div');
       tooltip.classList.add('tooltip');
       tooltip.id = `tooltip-${title.toLowerCase()}`;
       tooltip.textContent = title;
-      tooltip.style.top = `${rect.top - 3}px`; // adjusting for the slight padding the icons have at the bottom
-      tooltip.style.left = `${rect.left}px`;
-      tooltip.style.height = `${rect.height}px`;
-      tooltip.style.width = `${rect.width}px`;
 
       const addTooltip = () => {
+        if (a.querySelector('div.tooltip') !== null) return;
+
+        const rect = a.getBoundingClientRect();
+        tooltip.style.top = `${rect.top + rect.height}px`;
+        tooltip.style.left = `${rect.left}px`;
+        tooltip.style.width = `${rect.width}px`;
         a.appendChild(tooltip);
       };
 
@@ -26,8 +24,7 @@ if (socials.length > 0) {
       };
 
       a.addEventListener('mouseover', addTooltip);
-      a.addEventListener('mouseout', removeTooltip);
-      tooltip.addEventListener('mouseover', addTooltip);
+      a.addEventListener('mouseleave', removeTooltip);
     }
   });
 }
